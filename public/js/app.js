4f5ddd9b70ee40e61642f91c27d211801c8a5c92
Kalle Kuurila <kalle.kuurila@gmail.com>
2017-08-10 18:54:57 +0300

    Change double quotes to single quotes to make linter happy

--- public/js/app.js
+++ public/js/app.js
@@ -7,56 +7,56 @@ client.configure(feathers.socketio(socket));
 // Get the service for our `positions` endpoint
 const positions = client.service('positions');
 
-$(".power-button button").click(function(){
-  $.get("/movecam/q?data=power::" + document.getElementById('videoinput').value);
+$('.power-button button').click(function(){
+  $.get('/movecam/q?data=power::' + document.getElementById('videoinput').value);
 });
 
-$(".menu-button button").click(function(){
-  $.get("/movecam/c?data=menuToggle::" + document.getElementById('videoinput').value);
+$('.menu-button button').click(function(){
+  $.get('/movecam/c?data=menuToggle::' + document.getElementById('videoinput').value);
 });
 
 $(".menu-ok button").click(function(){
-  $.get("/movecam/c?data=menuOK::" + document.getElementById('videoinput').value);
+  $.get('/movecam/c?data=menuOK::' + document.getElementById('videoinput').value);
 });
 
 $(".menu-back button").click(function(){
-  $.get("/movecam/c?data=menuBack::" + document.getElementById('videoinput').value);
+  $.get('/movecam/c?data=menuBack::' + document.getElementById('videoinput').value);
 });
 
-$(".up-button button").bind("mousedown touchstart", function(){
-  $.get("/movecam/c?data=moveUp::" + document.getElementById('videoinput').value);
-}).bind("mouseup touchend", function(){
-  $.get("/movecam/c?data=moveStop::" + document.getElementById('videoinput').value);
+$('.up-button button').bind('mousedown touchstart', function(){
+  $.get('/movecam/c?data=moveUp::' + document.getElementById('videoinput').value);
+}).bind('mouseup touchend', function(){
+  $.get('/movecam/c?data=moveStop::' + document.getElementById('videoinput').value);
 });
 
-$(".left-button button").bind("mousedown touchstart", function(){
-  $.get("/movecam/c?data=moveLeft::" + document.getElementById('videoinput').value);
-}).bind("mouseup touchend", function(){
-  $.get("/movecam/c?data=moveStop::" + document.getElementById('videoinput').value);
+$('.left-button button').bind('mousedown touchstart', function(){
+  $.get('/movecam/c?data=moveLeft::' + document.getElementById('videoinput').value);
+}).bind('mouseup touchend', function(){
+  $.get('/movecam/c?data=moveStop::' + document.getElementById('videoinput').value);
 });
 
-$(".right-button button").bind("mousedown touchstart", function(){
-  $.get("/movecam/c?data=moveRight::" + document.getElementById('videoinput').value);
-}).bind("mouseup touchend", function(){
-  $.get("/movecam/c?data=moveStop::" + document.getElementById('videoinput').value);
+$('.right-button button').bind('mousedown touchstart', function(){
+  $.get('/movecam/c?data=moveRight::' + document.getElementById('videoinput').value);
+}).bind('mouseup touchend', function(){
+  $.get('/movecam/c?data=moveStop::' + document.getElementById('videoinput').value);
 });
 
-$(".down-button button").bind("mousedown touchstart", function(){
-  $.get("/movecam/c?data=moveDown::" + document.getElementById('videoinput').value);
-}).bind("mouseup touchend", function(){
-  $.get("/movecam/c?data=moveStop::" + document.getElementById('videoinput').value);
+$('.down-button button').bind('mousedown touchstart', function(){
+  $.get('/movecam/c?data=moveDown::' + document.getElementById('videoinput').value);
+}).bind('mouseup touchend', function(){
+  $.get('/movecam/c?data=moveStop::' + document.getElementById('videoinput').value);
 });
 
-$(".zoom-tele button").bind("mousedown touchstart", function(){
-  $.get("/movecam/c?data=zoomTeleStd::" + document.getElementById('videoinput').value);
-}).bind("mouseup touchend", function(){
-  $.get("/movecam/c?data=zoomStop::" + document.getElementById('videoinput').value);
+$('.zoom-tele button').bind('mousedown touchstart', function(){
+  $.get('/movecam/c?data=zoomTeleStd::' + document.getElementById('videoinput').value);
+}).bind('mouseup touchend', function(){
+  $.get('/movecam/c?data=zoomStop::' + document.getElementById('videoinput').value);
 });
 
-$(".zoom-wide button").bind("mousedown touchstart", function(){
-  $.get("/movecam/c?data=zoomWideStd::" + document.getElementById('videoinput').value);
-}).bind("mouseup touchend", function(){
-  $.get("/movecam/c?data=zoomStop::" + document.getElementById('videoinput').value);
+$('.zoom-wide button').bind('mousedown touchstart', function(){
+  $.get('/movecam/c?data=zoomWideStd::' + document.getElementById('videoinput').value);
+}).bind('mouseup touchend', function(){
+  $.get('/movecam/c?data=zoomStop::' + document.getElementById('videoinput').value);
 });
 
 function addCameraPosition(position) {
@@ -82,14 +82,14 @@ document.getElementById('position-mem').addEventListener('submit', function(ev) 
     name: sourceName.value
   });
 
-  camName.value = "";
+  sourceName.value = '';
   ev.preventDefault();
 });
 
 $(document).on('click', 'button[type=memory-button]', function() {
-  if (this.value.split("::")[2] === "0") {
-    $.get("/tbd/p?data=0"); // this needs own service to call atom switcher
+  if (this.value.split('::')[2] === '0') {
+    $.get('/tbd/p?data=0'); // this needs own service to call atom switcher
   } else {
-    $.get("/movecam/p?data=\"" + this.value + "\"");
+    $.get('/movecam/p?data="' + this.value + '"');
   }
 });
