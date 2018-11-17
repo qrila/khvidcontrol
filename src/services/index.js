1da4f1eb0403e49ce848bd0fc2fb06b4e1659f7a
Sebastian-Panula <35154108+Sebastian-Panula@users.noreply.github.com>
2018-11-17 14:02:53 +0200

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const movecam = require('./movecam/movecam.service.js');
 const positions = require('./positions/positions.service.js');
 const videoinputs = require('./videoinputs/videoinputs.service.js');
 const mixerinputs = require('./mixerinputs/mixerinputs.service.js');
+const mixerftb = require('./mixerftb/mixerftb.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(movecam);
   app.configure(positions);
   app.configure(videoinputs);
   app.configure(mixerinputs);
+  app.configure(mixerftb);
 };
