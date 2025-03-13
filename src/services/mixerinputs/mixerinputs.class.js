38162940f15944c98e8f666d0889d8fd06d6a5da
Kalle Kuurila <kalle.kuurila@gmail.com>
2025-03-13 20:51:08 +0200

    Check if there is mixer

--- src/services/mixerinputs/mixerinputs.class.js
+++ src/services/mixerinputs/mixerinputs.class.js
@@ -2,47 +2,50 @@
 
 /* eslint-disable no-unused-vars */
 class Service {
-  constructor (options) {
+  constructor(options) {
     this.options = options || {};
   }
 
-  get (data) {
+  get(data) {
     return new Promise((resolve, reject) => {
       const app = this.options.app;
       const input = JSON.parse(data);
       const atem = this.options.atem;
 
-      mixerIP(app).then( (mixerIP) => {
-        if(atem.state.model) {
-          atemCmd(input, atem);
-        } else {
-          atem.connect(mixerIP);
-          atem.on('connect', () => {
+      mixerIP(app).then((videomixerIP) => {
+        if (videomixerIP) {
+          if (atem.state.model) {
             atemCmd(input, atem);
-          });
+          } else {
+            atem.connect(videomixerIP);
+            atem.on("connect", () => {
+              atemCmd(input, atem);
+            });
+          }
         }
 
-        resolve('200');
-        reject('no mixer');
+        resolve("200");
+        reject("no mixer");
       });
     });
   }
 }
 
 async function mixerIP(app) {
-  const mixer = await app.service('videomixer').find();
-  return(mixer.data[0].videomixerIP);
+  const mixer = await app.service("videomixer").find();
+  if (!mixer.data.length) return null;
+  return mixer.data[0].videomixerIP;
 }
 
 async function atemCmd(input, atem) {
-  if(input.mixerAUX) {
+  if (input.mixerAUX) {
     // set aux to program output or input number 1
-    const auxMode = input.mixerAUX === 'program' ? 10010 : 1;
+    const auxMode = input.mixerAUX === "program" ? 10010 : 1;
     return atem.changeAuxInput(0, auxMode);
-  } else if(input.cut) {
+  } else if (input.cut) {
     // execute video cut transition
     return atem.cutTransition(0);
-  } else if(input.fadeToBlack) {
+  } else if (input.fadeToBlack) {
     // execute fade to black
     return atem.fadeToBlack(0);
   } else {
